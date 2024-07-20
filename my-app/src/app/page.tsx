48ffe7fbbbd6a51c24cb2f7e8d34efbab936b690
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-07-20 17:25:26 +0530

    test

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -66,12 +66,12 @@ export default function Main() {
         
 
       </section>
-
+{/* 
       <section className="h-screen w-full bg-neutral-950   rounded-md !overflow-visible relative flex flex-col items-center antialiased">
         <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]">
           <Footer />
         </div>
-      </section>
+      </section> */}
 
      
 
--- my-app/src/components/InfiniteMovingCards.tsx
+++ my-app/src/components/InfiniteMovingCards.tsx
@@ -79,11 +79,11 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full  relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
             style={{
               background: "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
-            key={item.index}
+            key={item?.index}
           >
             <blockquote>
               <div
@@ -91,15 +91,15 @@ export const InfiniteMovingCards = ({
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
               <span className="relative z-20 text-lg  leading-[1.6] text-gray-100  font-semibold">
-                {item.desc}
+                {item?.desc}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center w-full ">
                 <span className="flex flex-col gap-1">
                   <span className="text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.company}
+                    {item?.company}
                   </span>
                   <span className="text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
+                    {item?.title}
                   </span>
                 </span>
               </div>
